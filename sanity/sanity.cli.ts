00fad4b83f448081359bcabd0ba908a504a5c2d6
Etem Utku Oylum <utkuoylum@gmail.com>
2024-05-22 04:13:47 +0200

    navigation and sanity setup

---
+++ sanity/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'digy4ki7',
+    dataset: 'production'
+  }
+})
---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,9 @@
+export default function Navbar( ) {
+    return (
+        <nav className="w-full relative flex items-center justify-between max-w-2xl mx-auto px-4 py-5">
+            
+            <div>1</div>
+            <div>2</div>
+        </nav>
+    )
+}
