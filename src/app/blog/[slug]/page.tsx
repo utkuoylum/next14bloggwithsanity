bc4f277f041b5f6092ac450b0cdd1a65b49ce394
Etem Utku Oylum <utkuoylum@Etems-MacBook-Air.local>
2024-07-07 09:46:47 +0200

    blog implement

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,5 +1,7 @@
 import { fullBlog } from "@/app/lib/interface"
-import { client } from "@/app/lib/sanity"
+import { client, urlFor } from "@/app/lib/sanity"
+import { PortableText } from "@portabletext/react"
+import Image from "next/image"
 
 async function getData(slug: string) {
     const query = `*[_type == 'blog' && slug.current == '${slug}']{
@@ -24,6 +26,11 @@ export default async function BlogArticle({params}: {params: {slug: string}}) {
                 <span className="block text-base text-center text-primary font-semibold tracking-wide uppercase">Utku Oylum - Blog</span>
                 <span className="mt-2 block text-3xl text-center leading-8 font-bold tracking-tight sm:text-4xl">{data.title}</span>
             </h1>
+
+            <Image src={urlFor(data.titleImage).url()} alt='article image' width={800} height={800} className="rounded-lg mt-8" priority></Image>
+            <div className="mt-16 prose prose-blue prose-xl dark:prose-invert prose-headings:underline prose-li:marker:text-primary">
+                <PortableText value={data.content}></PortableText>
+            </div>
         </div>
     )
 }
